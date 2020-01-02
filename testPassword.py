f9aafd04c7b07bb099a110d688ec65559d1172c8
Heiko Bühler <hbuehler@stud.hs-offenburg.de>
2020-01-02 23:13:24 +0100

    Add test cases

--- testPassword.py
+++ testPassword.py
@@ -1,9 +1,30 @@
 import unittest
 import Password
 
+cleartext_password = b"myS3cr3tP455w0Rd"
+
 class testPassword(unittest.TestCase):
-    def test1(self):
-        True
+
+    def setUp(self):
+        self.pw_hasher = Password.Password()
+
+    def test_valid_hash(self):
+        hash = self.pw_hasher.hash_password(cleartext_password)
+        self.assertTrue(
+            self.pw_hasher.hash_check(
+                cleartext_password,
+                hash
+        ))
+
+    def test_invalid_hash(self):
+        hash = self.pw_hasher.hash_password(cleartext_password)
+        invalid_hash = b'$2b$12$o9kR21YMqEMofk2EEx2Y9OQeuaaQc2LbrYTtRNFEHwEh.NVcN.2lK'
+        self.assertFalse(
+            self.pw_hasher.hash_check(
+                cleartext_password,
+                invalid_hash
+        ))
+
 
 if __name__ == '__main__':
     unittest.main()
